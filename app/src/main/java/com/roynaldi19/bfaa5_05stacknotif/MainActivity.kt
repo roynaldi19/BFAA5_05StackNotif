fa9eadbb59c8fa785f0f19d69be32fa71cdef215
roynaldi19 <roynaldi19@gmail.com>
2021-02-19 20:38:14 +0700

    Initial commit

---
+++ app/src/main/java/com/roynaldi19/bfaa5_05stacknotif/MainActivity.kt
@@ -0,0 +1,100 @@
+package com.roynaldi19.bfaa5_05stacknotif
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.graphics.BitmapFactory
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.core.app.NotificationCompat
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    private var idNotification = 0
+
+    private val stackNotif = ArrayList<NotificationItem>()
+
+    companion object {
+        private const val CHANNEL_NAME = "dicoding channel"
+        private const val GROUP_KEY_EMAILS = "group_key_emails"
+        private const val NOTIFICATION_REQUEST_CODE = 200
+        private const val MAX_NOTIFICATION = 2
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnSend.setOnClickListener {
+            val sender = edtSender.text.toString()
+            val message = edtMessage.text.toString()
+            if (sender.isEmpty() || message.isEmpty()) {
+                Toast.makeText(this@MainActivity, "Data harus diisi", Toast.LENGTH_SHORT).show()
+            } else {
+                stackNotif.add(NotificationItem(idNotification, sender, message))
+                sendNotif()
+                idNotification++
+                edtSender.setText("")
+                edtMessage.setText("")
+
+                //tutup keyboard ketika tombol diklik
+                val methodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                methodManager.hideSoftInputFromWindow(edtMessage.windowToken, 0)
+            }
+        }
+    }
+
+    private fun sendNotif() {
+        val mNotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val largeIcon = BitmapFactory.decodeResource(resources, R.drawable.ic_baseline_notifications_24)
+        val mBuilder: NotificationCompat.Builder
+
+        //Melakukan pengecekan jika idNotification lebih kecil dari Max Notif
+        val CHANNEL_ID = "channel_01"
+        if (idNotification < MAX_NOTIFICATION) {
+            mBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+                    .setContentTitle("New Email from " + stackNotif[idNotification].sender)
+                    .setContentText(stackNotif[idNotification].message)
+                    .setSmallIcon(R.drawable.ic_baseline_mail_24)
+                    .setLargeIcon(largeIcon)
+                    .setGroup(GROUP_KEY_EMAILS)
+                    .setAutoCancel(true)
+        } else {
+            val inboxStyle = NotificationCompat.InboxStyle()
+                    .addLine("New Email from " + stackNotif[idNotification].sender)
+                    .addLine("New Email from " + stackNotif[idNotification - 1].sender)
+                    .setBigContentTitle("$idNotification new emails")
+                    .setSummaryText("mail@dicoding")
+            mBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+                    .setContentTitle("$idNotification new emails")
+                    .setContentText("mail@dicoding.com")
+                    .setSmallIcon(R.drawable.ic_baseline_mail_24)
+                    .setGroup(GROUP_KEY_EMAILS)
+                    .setGroupSummary(true)
+                    .setStyle(inboxStyle)
+                    .setAutoCancel(true)
+        }
+        /*
+        Untuk android Oreo ke atas perlu menambahkan notification channel
+        Materi ini akan dibahas lebih lanjut di modul extended
+         */
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            /* Create or update. */
+            val channel = NotificationChannel(CHANNEL_ID,
+                    CHANNEL_NAME,
+                    NotificationManager.IMPORTANCE_DEFAULT)
+
+            mBuilder.setChannelId(CHANNEL_ID)
+
+            mNotificationManager.createNotificationChannel(channel)
+        }
+
+        val notification = mBuilder.build()
+
+        mNotificationManager.notify(idNotification, notification)
+    }
+}
---
+++ app/src/main/java/com/roynaldi19/bfaa5_05stacknotif/NotificationItem.kt
@@ -0,0 +1,7 @@
+package com.roynaldi19.bfaa5_05stacknotif
+
+data class NotificationItem(
+    var id: Int,
+    var sender: String?,
+    var message: String?
+)
